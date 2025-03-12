d775b3989b7ef0029a70ad7db042408da40434c3
A Anand <a.anand.91119@gmail.com>
2025-03-12 22:29:17 +0530

    Adding base package

--- build.gradle.kts
+++ build.gradle.kts
@@ -24,16 +24,26 @@ repositories {
 	mavenCentral()
 }
 
+object Version {
+	const val OPEN_API_DOC_VERSION = "2.3.0"
+}
+
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-actuator")
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("org.springframework.kafka:spring-kafka")
+	implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:${Version.OPEN_API_DOC_VERSION}")
+
 	compileOnly("org.projectlombok:lombok")
+
 	developmentOnly("org.springframework.boot:spring-boot-devtools")
 	developmentOnly("org.springframework.boot:spring-boot-docker-compose")
+
 	annotationProcessor("org.projectlombok:lombok")
+
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("org.springframework.kafka:spring-kafka-test")
+
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
--- src/main/java/dev/notyouraverage/project/one/http/asynchronous_http_server/AsynchronousHttpServerApplication.java
+++ src/main/java/dev/notyouraverage/project/one/http/asynchronous_http_server/AsynchronousHttpServerApplication.java
@@ -2,12 +2,14 @@ package dev.notyouraverage.project.one.http.asynchronous_http_server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"dev.notyouraverage.project.base", "dev.notyouraverage.project.one"})
 public class AsynchronousHttpServerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(AsynchronousHttpServerApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(AsynchronousHttpServerApplication.class, args);
+    }
 
 }
