aa90befc354b1a06e62f6584af9a9631dea36daf
A Anand <a.anand.91119@gmail.com>
2025-03-13 21:37:32 +0530

    spotless formatting

--- src/main/java/dev/notyouraverage/project/one/http/asynchronous_http_server/services/impl/MainServiceImpl.java
+++ src/main/java/dev/notyouraverage/project/one/http/asynchronous_http_server/services/impl/MainServiceImpl.java
@@ -9,11 +9,9 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.core.KafkaTemplate;
-import org.springframework.kafka.support.SendResult;
 import org.springframework.stereotype.Service;
 
 import java.util.UUID;
-import java.util.concurrent.CompletableFuture;
 
 @Slf4j
 @Service
